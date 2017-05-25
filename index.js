27cdeb088d391ccf7cbbef5771608c6e8bfcf309
Matt Wade <romellem.git@gmail.com>
2017-05-25 12:25:35 -0500

    Fixes linting errors

--- index.js
+++ index.js
@@ -15,38 +15,38 @@ module.exports = opts => buf => {
 	}
 
 	// Default is to strip all markers from output file
-	opts.stripCom = typeof opts.stripCom !== 'undefined' ? opts.stripCom : true;
-	opts.stripExif = typeof opts.stripExif !== 'undefined' ? opts.stripExif : true;
-	opts.stripIptc = typeof opts.stripIptc !== 'undefined' ? opts.stripIptc : true;
-	opts.stripIcc = typeof opts.stripIcc !== 'undefined' ? opts.stripIcc : true;
-	opts.stripXmp = typeof opts.stripXmp !== 'undefined' ? opts.stripXmp : true;
+	opts.stripCom = typeof opts.stripCom === 'undefined' ? true : opts.stripCom;
+	opts.stripExif = typeof opts.stripExif === 'undefined' ? true : opts.stripExif;
+	opts.stripIptc = typeof opts.stripIptc === 'undefined' ? true : opts.stripIptc;
+	opts.stripIcc = typeof opts.stripIcc === 'undefined' ? true : opts.stripIcc;
+	opts.stripXmp = typeof opts.stripXmp === 'undefined' ? true : opts.stripXmp;
 
 	const args = [
 		'--stdin',
 		'--stdout'
 	];
 
-	// strip Comment markers from output file
+	// Strip Comment markers from output file
 	if (opts.stripCom) {
 		args.push('--strip-com');
 	}
 
-	// strip Exif markers from output file
+	// Strip Exif markers from output file
 	if (opts.stripExif) {
 		args.push('--strip-exif');
 	}
 
-	// strip IPTC/Photoshop (APP13) markers from output file
+	// Strip IPTC/Photoshop (APP13) markers from output file
 	if (opts.stripIptc) {
 		args.push('--strip-iptc');
 	}
 
-	// strip ICC profile markers from output file
+	// Strip ICC profile markers from output file
 	if (opts.stripIcc) {
 		args.push('--strip-icc');
 	}
 
-	// strip XMP markers markers from output file
+	// Strip XMP markers markers from output file
 	if (opts.stripXmp) {
 		args.push('--strip-xmp');
 	}
