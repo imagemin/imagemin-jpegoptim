41300c3c590297d1868f5707024a88c0ba666c9d
Matt Wade <romellem.git@gmail.com>
2017-05-25 13:44:54 -0500

    Requires `stripAll` option to be false to set other "strip" flags
    For instance, running:
    
    ```
    imageminJpegoptim({
        stripAll: true,
        stripIcc: false
    })
    ```
    
    Doesn't really make sense, so the `stripIcc` will be ignored.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const isJpg = require('is-jpg');
 const jpegoptim = require('jpegoptim-bin');
 
 module.exports = opts => buf => {
-	opts = Object.assign({}, opts);
+	opts = Object.assign({
+		stripAll: true
+	}, opts);
 
 	if (!Buffer.isBuffer(buf)) {
 		return Promise.reject(new TypeError(`Expected a Buffer, got ${typeof buf}`));
@@ -14,41 +16,33 @@ module.exports = opts => buf => {
 		return Promise.resolve(buf);
 	}
 
-	// Default is to strip all markers from output file
-	opts.stripCom = typeof opts.stripCom === 'undefined' ? true : opts.stripCom;
-	opts.stripExif = typeof opts.stripExif === 'undefined' ? true : opts.stripExif;
-	opts.stripIptc = typeof opts.stripIptc === 'undefined' ? true : opts.stripIptc;
-	opts.stripIcc = typeof opts.stripIcc === 'undefined' ? true : opts.stripIcc;
-	opts.stripXmp = typeof opts.stripXmp === 'undefined' ? true : opts.stripXmp;
-
 	const args = [
 		'--stdin',
 		'--stdout'
 	];
 
-	// Strip Comment markers from output file
-	if (opts.stripCom) {
-		args.push('--strip-com');
-	}
+	if (opts.stripAll) {
+		args.push('--strip-all');
+	} else {
+		if (opts.stripCom) {
+			args.push('--strip-com');
+		}
 
-	// Strip Exif markers from output file
-	if (opts.stripExif) {
-		args.push('--strip-exif');
-	}
+		if (opts.stripExif) {
+			args.push('--strip-exif');
+		}
 
-	// Strip IPTC/Photoshop (APP13) markers from output file
-	if (opts.stripIptc) {
-		args.push('--strip-iptc');
-	}
+		if (opts.stripIptc) {
+			args.push('--strip-iptc');
+		}
 
-	// Strip ICC profile markers from output file
-	if (opts.stripIcc) {
-		args.push('--strip-icc');
-	}
+		if (opts.stripIcc) {
+			args.push('--strip-icc');
+		}
 
-	// Strip XMP markers markers from output file
-	if (opts.stripXmp) {
-		args.push('--strip-xmp');
+		if (opts.stripXmp) {
+			args.push('--strip-xmp');
+		}
 	}
 
 	if (opts.progressive) {
